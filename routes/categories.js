578023273499dbcfaf1e72071e4b8a72ac3906ed
Anas Riza <nameisriza123boy@gmail.com>
2025-02-02 09:23:36 +0530

    backend

---
+++ routes/categories.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const Category = require('../models/Category');
+
+
+// Get All Categories
+router.get("/",async(req,res) =>{
+    try {
+        const categories = await Category.find()
+        res.json(categories)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+
+// Get a single Category By ID
+router.get("/:id", async (req, res) => {
+    try {
+      const category = await Category.findById(req.params.id);
+      if (!category) {
+        return res.status(404).json({ message: "Category  not found" });
+      }
+      res.json(category);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  // create a Category posts
+
+router.post("/", async (req, res) => {
+    const category = new Category({
+      name: req.body.name,
+      slug: req.body.slug,
+      description: req.body.description,
+    });
+    try {
+      const newCategory = await category.save();
+      res.status(201).json(newCategory);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+  // Update an existing Category
+router.put("/:id", async (req, res) => {
+    try {
+      const category = await Category.findById(req.params.id);
+      if (!category) {
+        return res.status(404).json({ message: "Category not found 2" });
+      }
+  
+      category.name = req.body.name || category.name;
+      category.slug = req.body.slug || category.slug;
+      category.description = req.body.description || category.description;
+      category.updatedAt = Date.now();
+  
+      const updatedCategory = await category.save();
+      res.json(updatedCategory);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+
+  // Delete a Post
+router.delete("/:id", async (req, res) => {
+    try {
+      const category = await Category.findById(req.params.id);
+      if (!category) {
+        return res.status(404).json({ message: "Category not found 3" });
+      }
+      await Category.findByIdAndDelete(category._id);
+      res.json({ message: " Category deleted" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
+  module.exports = router;
